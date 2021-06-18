c34a0fd9074f91702010344b7ac8ae324511c4c1
Abdou Diaw <diaw@Abdous-MacBook-Pro.local>
2021-06-18 08:52:47 -0400

    new input file

---
+++ src/types.f90
@@ -0,0 +1,5 @@
+module mod_types
+    use, intrinsic :: iso_fortran_env, only: rp => real32, &
+                                             dp => real64, &
+                                             sp => int32
+end module mod_types
